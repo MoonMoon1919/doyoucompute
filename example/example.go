84dbada674c54e4826a4af2e3bea0169f7da94e6
Maxwell Moon <moon.maxwell@gmail.com>
2025-07-17 06:55:03 -0700

    feat: Start on domain interface

--- example/example.go
+++ example/example.go
@@ -7,57 +7,113 @@ import (
 	"github.com/MoonMoon1919/doyoucompute"
 )
 
-func main() {
+func manualRoute() doyoucompute.Document {
 	file, err := os.Open("../testdocs/partial.md")
 	if err != nil {
 		panic(err)
 	}
 
-	section := doyoucompute.Section{
-		Name: "Intro",
+	document := doyoucompute.Document{
+		Name: "MY DOC",
 		Content: []doyoucompute.Node{
 			doyoucompute.Paragraph{
 				Items: []doyoucompute.Node{
-					doyoucompute.Text("cool text bro"),
-					doyoucompute.Code("very cool code"),
-					doyoucompute.Link{
-						Text: "Some Link",
-						Url:  "https://example.com",
+					doyoucompute.Text("I am an introduction paragraph"),
+				},
+			},
+			doyoucompute.Section{
+				Name: "Intro",
+				Content: []doyoucompute.Node{
+					doyoucompute.Paragraph{
+						Items: []doyoucompute.Node{
+							doyoucompute.Text("cool text bro"),
+							doyoucompute.Code("very cool code"),
+							doyoucompute.Link{
+								Text: "Some Link",
+								Url:  "https://example.com",
+							},
+						},
 					},
-				},
-			},
-			doyoucompute.Header{Content: "Things"},
-			doyoucompute.BlockQuote("Here i am blockin' on my own"),
-			doyoucompute.Remote{Reader: file},
-			doyoucompute.Executable{
-				Shell: "sh",
-				Cmd:   []string{"echo", "hello", "world"},
-			},
-			doyoucompute.CodeBlock{
-				BlockType: "json",
-				Cmd:       []string{`{"key": "value"}`},
-			},
-			doyoucompute.Table{
-				Headers: []string{"my", "cool", "table"},
-				Items: []doyoucompute.Node{
-					doyoucompute.TableRow{Values: []string{"some", "cool", "content"}},
-					doyoucompute.TableRow{Values: []string{"more", "nice", "stuff"}},
-					doyoucompute.TableRow{Values: []string{"very", "great", "table"}},
-				},
-			},
-			doyoucompute.List{
-				TypeOfList: doyoucompute.NUMBERED,
-				Items: []doyoucompute.Node{
-					doyoucompute.Text("first item"),
-					doyoucompute.Code("npm i"),
+					doyoucompute.BlockQuote("Here i am blockin' on my own"),
+					doyoucompute.Remote{Reader: file},
+					doyoucompute.Executable{
+						Shell: "sh",
+						Cmd:   []string{"echo", "hello", "world"},
+					},
+					doyoucompute.CodeBlock{
+						BlockType: "json",
+						Cmd:       []string{`{"key": "value"}`},
+					},
+					doyoucompute.Table{
+						Headers: []string{"my", "cool", "table"},
+						Items: []doyoucompute.Node{
+							doyoucompute.TableRow{Values: []string{"some", "cool", "content"}},
+							doyoucompute.TableRow{Values: []string{"more", "nice", "stuff"}},
+							doyoucompute.TableRow{Values: []string{"very", "great", "table"}},
+						},
+					},
+					doyoucompute.List{
+						TypeOfList: doyoucompute.NUMBERED,
+						Items: []doyoucompute.Node{
+							doyoucompute.Text("first item"),
+							doyoucompute.Code("npm i"),
+						},
+					},
 				},
 			},
 		},
 	}
 
-	renderer := doyoucompute.Markdown{}
+	return document
+}
 
-	rendered, _ := renderer.Render(section)
+func builderRoute() doyoucompute.Document {
+	file, err := os.Open("../testdocs/partial.md")
+	if err != nil {
+		panic(err)
+	}
+
+	document := doyoucompute.NewDocument("MY DOC")
+	docIntro := doyoucompute.NewParagraph().Next(doyoucompute.Text("I am an introduction paragraph"))
+
+	document.AddIntro(docIntro)
+
+	// Build the section
+	section := doyoucompute.NewSection("Intro")
+	intro := doyoucompute.NewParagraph().Next(doyoucompute.Text("cool text bro")).Next(doyoucompute.Code("very cool code")).Next(doyoucompute.Link{
+		Text: "Some Link",
+		Url:  "https://example.com",
+	})
+
+	section.AddIntro(intro)
+	section.AddBlockQuote("Here i am blockin' on my own")
+	section.AddRemoteContent(doyoucompute.Remote{Reader: file})
+	section.AddCodeBlock("sh", []string{"echo", "hello", "world"}, true)
+	section.AddCodeBlock("json", []string{`{"key": "value"}`}, false)
+	section.AddTable(
+		[]string{"my", "cool", "table"},
+		[]doyoucompute.Node{
+			doyoucompute.TableRow{Values: []string{"some", "cool", "content"}},
+			doyoucompute.TableRow{Values: []string{"more", "nice", "stuff"}},
+			doyoucompute.TableRow{Values: []string{"very", "great", "table"}},
+		},
+	)
+	section.AddList(doyoucompute.NUMBERED, []doyoucompute.Node{
+		doyoucompute.Text("first item"),
+		doyoucompute.Code("npm i"),
+	})
+
+	document.AddSection(section)
+
+	return document
+}
+
+func main() {
+	renderer := doyoucompute.Markdown{}
+	document := builderRoute()
+	rendered, _ := renderer.Render(document)
+
+	manualRoute() // So go stops yelling at me
 
 	fmt.Print(rendered)
 }
--- structure.go
+++ structure.go
@@ -42,9 +42,23 @@ func (l List) Children() []Node { return l.Items }
 
 func (l List) Identifer() string { return "" }
 
+func (l *List) Push(item Node) {
+	l.Items = append([]Node{item}, l.Items...)
+}
+
+func (l *List) Append(item Node) {
+	l.Items = append(l.Items, item)
+}
+
 // A container that allows us to render content with paragraph semantics
 type Paragraph struct {
 	Items []Node // how can we only allow content types here so people don't do awkward things
+}
+
+func NewParagraph() *Paragraph {
+	return &Paragraph{
+		Items: make([]Node, 0),
+	}
 }
 
 func (p Paragraph) Type() ContentType { return ParagraphType }
@@ -53,8 +67,9 @@ func (p Paragraph) Children() []Node { return p.Items }
 
 func (p Paragraph) Identifer() string { return "" }
 
-func (p Paragraph) Write(content Node) {
+func (p *Paragraph) Next(content Node) *Paragraph {
 	p.Items = append(p.Items, content)
+	return p
 }
 
 // A single section has a name and 1..N items of content
@@ -63,23 +78,67 @@ type Section struct {
 	Content []Node
 }
 
+func NewSection(name string) Section {
+	return Section{
+		Name:    name,
+		Content: make([]Node, 0),
+	}
+}
+
 func (s Section) Children() []Node { return s.Content }
 
 func (s Section) Type() ContentType { return SectionType }
 
 func (s Section) Identifer() string { return s.Name }
 
-func (s Section) AddSection(section Section) {
-	// TODO: Ordering is important
+func (s *Section) AddIntro(content *Paragraph) {
+	s.Content = append([]Node{content}, s.Content...)
+}
+
+func (s *Section) AddSection(section Section) {
 	s.Content = append(s.Content, section)
 }
 
-func (s Section) WriteSection(name string) *Section {
-	section := Section{Name: name}
+func (s *Section) AddParagraph(paragraph Paragraph) {
+	s.Content = append(s.Content, paragraph)
+}
 
-	s.Content = append(s.Content, &section)
+func (s *Section) AddTable(headers []string, rows []Node) {
+	table := Table{Headers: headers, Items: rows}
 
-	return &section
+	s.Content = append(s.Content, table)
+}
+
+func (s *Section) AddList(listType ListTypeE, items []Node) {
+	list := List{TypeOfList: listType, Items: items}
+
+	s.Content = append(s.Content, list)
+}
+
+func (s *Section) AddCodeBlock(blockType string, cmd []string, executable bool) {
+	var newContent Node
+
+	if executable {
+		newContent = Executable{
+			Shell: blockType,
+			Cmd:   cmd,
+		}
+	} else {
+		newContent = CodeBlock{
+			BlockType: blockType,
+			Cmd:       cmd,
+		}
+	}
+
+	s.Content = append(s.Content, newContent)
+}
+
+func (s *Section) AddBlockQuote(value string) {
+	s.Content = append(s.Content, BlockQuote(value))
+}
+
+func (s *Section) AddRemoteContent(value Node) {
+	s.Content = append(s.Content, value)
 }
 
 // A document contains many sections
@@ -88,21 +147,23 @@ type Document struct {
 	Content []Node
 }
 
+func NewDocument(name string) Document {
+	return Document{
+		Name:    name,
+		Content: make([]Node, 0),
+	}
+}
+
 func (d Document) Type() ContentType { return DocumentType }
 
 func (d Document) Children() []Node { return d.Content }
 
 func (d Document) Identifer() string { return d.Name }
 
-func (d Document) AddSection(section Section) {
-	// TODO: Ordering is important
-	d.Content = append(d.Content, section)
+func (d *Document) AddIntro(content *Paragraph) {
+	d.Content = append([]Node{content}, d.Content...)
 }
 
-func (d Document) WriteSection(name string) *Section {
-	section := Section{Name: name}
-
-	d.Content = append(d.Content, &section)
-
-	return &section
+func (d *Document) AddSection(section Section) {
+	d.Content = append(d.Content, section)
 }
