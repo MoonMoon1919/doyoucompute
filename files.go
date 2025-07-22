a10a50b255f9648163e77ec67e5fc8c2e1e2a8c7
Maxwell Moon <moon.maxwell@gmail.com>
2025-07-22 06:29:21 -0700

    feat: Add file loading

--- files.go
+++ files.go
@@ -2,6 +2,15 @@ package doyoucompute
 
 import "io"
 
+func LoadFile(reader io.Reader) (string, error) {
+	content, err := io.ReadAll(reader)
+	if err != nil {
+		return "", err
+	}
+
+	return string(content), nil
+}
+
 func WriteFile(writer io.Writer, document Document, renderer Renderer[string]) error {
 	content, err := renderer.Render(document)
 	if err != nil {
--- repository.go
+++ repository.go
@@ -3,10 +3,22 @@ package doyoucompute
 import "os"
 
 type Repository interface {
+	Load(path string) (string, error)
 	Save(path string, document Document, renderer Renderer[string]) error
 }
 
 type FileRepository struct{}
+
+func (f FileRepository) Load(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+
+	defer file.Close()
+
+	return LoadFile(file)
+}
 
 func (f FileRepository) Save(path string, document Document, renderer Renderer[string]) error {
 	file, err := os.Create(path)
